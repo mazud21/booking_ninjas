c6cbad258240fd6464a3f4608f6929a0ab5c3fc4
mazud21 <hmazud@gmail.com>
2022-11-15 15:49:17 +0700

    update history

--- lib/view/history.dart
+++ lib/view/history.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class History extends StatefulWidget {
-
   @override
   State<History> createState() => _HistoryState();
 }
 
 class _HistoryState extends State<History> {
-
-  Widget myAppBarIcon(){
+  Widget myAppBarIcon() {
     return Container(
       width: 30,
       height: 30,
@@ -36,7 +35,8 @@ class _HistoryState extends State<History> {
                 padding: const EdgeInsets.all(0.0),
                 child: Center(
                   child: Text(
-                    /*_counter.toString()*/ '1',
+                    /*_counter.toString()*/
+                    '1',
                     style: TextStyle(fontSize: 10),
                   ),
                 ),
@@ -53,50 +53,85 @@ class _HistoryState extends State<History> {
     return Scaffold(
       appBar: AppBar(
         leading: Icon(CupertinoIcons.bars, color: Colors.black45),
-        title: Text('History', style: TextStyle(fontWeight: FontWeight.w500, color: Colors.black45),),
+        title: Text(
+          'History',
+          style: TextStyle(fontWeight: FontWeight.w500, color: Colors.black45),
+        ),
         elevation: 0,
         backgroundColor: Colors.transparent,
-        actions: [
-          myAppBarIcon()
-        ],
+        actions: [myAppBarIcon()],
       ),
       body: Container(
         padding: EdgeInsets.all(16),
         child: ListView(
           children: [
-        TextFormField(
-        decoration: InputDecoration(
-        labelText: "Search",
-          //fillColor: Colors.white,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: BorderSide(
-                color: Colors.white10
-            ),
-          ),
-          prefixIcon: Icon(Icons.search),
-          suffixIcon: InkWell(
-            onTap: () => '',
-            child: Icon(Icons.mic),
-          )
-        )
-    ),
+            TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Search",
+                    //fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide(color: Colors.white10),
+                    ),
+                    prefixIcon: Icon(Icons.search),
+                    suffixIcon: InkWell(
+                      onTap: () => '',
+                      child: Icon(Icons.mic),
+                    ))),
             SizedBox(
               height: 16,
             ),
-            Row(
-              children: [
-                Chip(
-
-                  label: Text(''),
-                  padding: EdgeInsets.all(16),
-                  backgroundColor: Color.fromRGBO(
-                      56, 187, 253, 0.4196078431372549),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                  avatar: Icon(CupertinoIcons.slider_horizontal_3),
-                )
-              ],
-            )
+            SizedBox(
+              height: Get.height * 0.07,
+              child: Row(
+                children: <Widget>[
+                  SizedBox(
+                    width: Get.width * 0.2,
+                    child: Chip(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)
+                      ),
+                      backgroundColor: Colors.blue,
+                      label: Icon(Icons.home),
+                      padding: EdgeInsets.all(8),
+                    ),
+                    /*Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.blue,
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Icon(CupertinoIcons.slider_horizontal_3)),
+                    ),*/
+                  ),
+                  SizedBox(
+                    width: Get.width * 0.7,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.all(4),
+                          child: Chip(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8)
+                            ),
+                            avatar: InkWell(
+                              onTap: () => null,
+                              child: Icon(Icons.close),
+                            ),
+                            backgroundColor: Colors.blue,
+                            label: Text('data '),
+                            padding: EdgeInsets.all(8),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
